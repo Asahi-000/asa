03435274f9d8f9daaf3f806e2216897e97fecd3c
Asahi-000 <160554947+Asahi-000@users.noreply.github.com>
2024-10-15 10:20:13 +0800

    Add files via upload

---
+++ kvfarith/src/main/java/com/mycompany/kvfarith/Kvfarith.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kvfarith;
+
+/**
+ *
+ * @author CL3-PC12
+ */
+public class Kvfarith {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
