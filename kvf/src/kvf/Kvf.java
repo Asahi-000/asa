beaa7b75e86ab0e7e8bf0f12ae2135ec1d3d56e3
Asahi-000 <160554947+Asahi-000@users.noreply.github.com>
2024-10-15 08:24:18 +0800

    Add files via upload

---
+++ kvf/src/kvf/Kvf.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kvf;
+
+/**
+ *
+ * @author CL3-PC12
+ */
+public class Kvf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
